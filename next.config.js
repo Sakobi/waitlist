69bb5e68600f1acde400835c3e95f5f0716df585
Sergei Mikhailov <mikhaylovserj@gmail.com>
2025-11-08 14:15:26 +0800

    Add env config to embed Supabase variables in static build

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   },
   basePath: "",
   assetPrefix: "",
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '',
+  },
   outputFileTracingRoot: __dirname,
   webpack: (config, { isServer }) => {
     // Fix for potential module resolution issues
